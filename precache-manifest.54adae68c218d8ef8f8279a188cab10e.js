f56b342013abe21c92443ea96917d13ae6837409
Marius Gava <49473703+0XLR8@users.noreply.github.com>
2020-07-03 12:28:35 +0300

    Add files via upload

---
+++ precache-manifest.54adae68c218d8ef8f8279a188cab10e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15dcb462cee09886e61f13c7e2aef821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eea4bf452a79dae5e55",
+    "url": "/static/css/main.bcbef9a3.chunk.css"
+  },
+  {
+    "revision": "c9d12e80c3c467e75277",
+    "url": "/static/js/2.f98a5b7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f98a5b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eea4bf452a79dae5e55",
+    "url": "/static/js/main.622b6cd8.chunk.js"
+  },
+  {
+    "revision": "2dc9d719c56063e714a4",
+    "url": "/static/js/runtime-main.edf8ef9e.js"
+  }
+]);
